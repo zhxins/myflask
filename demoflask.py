12c911c9445434e4059fc3ac6d931cb11530813b
falleves <zhxin20@163.com>
2018-04-12 16:12:57 +0800

    first commit

---
+++ demoflask.py
@@ -0,0 +1,43 @@
+# *_* coding:utf-8 *_*
+from flask import Flask, redirect, flash
+from flask import render_template
+
+from forms import LoginForm
+
+app = Flask(__name__)
+app.config.from_object("config")
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/index')
+def index():
+    user = {"nickname": "zhxin"}
+    posts = [{
+        "author": {"nickname": "John"},
+        "body": "beautiful day in Portland"
+        }, {
+        "author": {"nickname": "Ajax"},
+        "body": "beautiful day in aruili"
+        }]
+    return render_template("index.html",
+                           title = "Home",
+                           user = user,
+                           posts = posts)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    # 验证通过
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect("/index")
+    return render_template("login.html", title = "Sign in", form = form, providers = app.config['OPENID_PROVIDERS'])
+
+
+if __name__ == '__main__':
+    app.run()
